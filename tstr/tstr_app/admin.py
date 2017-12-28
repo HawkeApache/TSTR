40fc7bbbc6eea062ee2049d9e52dcbf5c5eaf3ca
zdomcia <zdomcia@gmail.com>
2017-12-28 14:40:00 +0100

    fix problem with headers

--- tstr/tstr_app/admin.py
+++ tstr/tstr_app/admin.py
@@ -1,8 +1,14 @@
 from __future__ import with_statement
 
+import django
+import random
+import string
+from tstr.tstr_app.models import Student
+
 from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
@@ -24,9 +30,7 @@ class StudentAdmin(ImportExportModelAdmin):
         'process_import' for the actual import.
         '''
 
-        print("import action!")
         resource = self.get_import_resource_class()(**self.get_import_resource_kwargs(request, *args, **kwargs))
-
         context = self.get_import_context_data()
 
         import_formats = self.get_import_formats()
@@ -35,19 +39,11 @@ class StudentAdmin(ImportExportModelAdmin):
                          request.POST or None,
                          request.FILES or None)
 
-        print("przed ifem")
         if request.POST and form.is_valid():
             import_file = form.cleaned_data['import_file']
-            # first always write the uploaded file to disk as it may be a
-            # memory file or else based on settings upload handlers
-            tmp_storage = self.get_tmp_storage_class()()
             data = bytes()
             for chunk in (import_file.chunks()):
                 data += chunk
-
-            import random
-            import string
-            from tstr.tstr_app.models import Student
 
             tab = data.split(b'\r\n')
             for index, line in enumerate(tab):
@@ -64,7 +60,7 @@ class StudentAdmin(ImportExportModelAdmin):
                 password = ''.join(
                     random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
 
-                print(first_name, last_name, index, username, password, is_active_usos, sep=" ; ")
+                # print(first_name, last_name, index, username, password, is_active_usos, sep=" ; ")
 
                 try:
                     student = Student.objects.get(indeks=index)
@@ -80,6 +76,19 @@ class StudentAdmin(ImportExportModelAdmin):
                 student.save()
 
             return redirect('/admin/tstr_app/student')
+
+        if django.VERSION >= (1, 8, 0):
+            context.update(self.admin_site.each_context(request))
+        elif django.VERSION >= (1, 7, 0):
+            context.update(self.admin_site.each_context())
+
+        context['title'] = ("Import")
+        context['form'] = form
+        context['opts'] = self.model._meta
+        context['fields'] = [f.column_name for f in resource.get_user_visible_fields()]
+
+        request.current_app = self.admin_site.name
+        return TemplateResponse(request, [self.import_template_name], context)
 
 
 @admin.register(OpenQuestion)
--- tstr/tstr_app/resources.py
+++ tstr/tstr_app/resources.py
@@ -18,34 +18,6 @@ class StudentResource(resources.ModelResource):
     password = Field(column_name='password', attribute='password')
     username = Field(column_name='username', attribute='username')
 
-    def before_import(self, dataset, using_transactions, dry_run=False, **kwargs):
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11")
-        for data in dataset.dict:
-            first_name = force_text(data['imie'], 'utf-8')
-            last_name = force_text(data['nazwisko'], 'utf-8')
-            is_active_usos = 1
-            if data['rezygnacja'] == '1' or data['skreslony'] == '1':
-                is_active_usos = 0
-            username = 's' + force_text(data['indeks'], 'utf-8')
-            index = force_text(data['indeks'], 'utf-8')
-            password = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
-
-            try:
-                student = Student.objects.get(indeks=index)
-            except Student.DoesNotExist:
-                student = Student.objects.create_user(username=username, indeks=index)
-
-            student.indeks = index
-            student.username = username
-            student.first_name = first_name
-            student.last_name = last_name
-            student.is_active_USOS = is_active_usos
-            student.password = password
-            student.save()
-
-        return super(StudentResource, self).before_import(dataset, using_transactions, dry_run, **kwargs)
-
-
     class Meta:
         fields = ('indeks', 'is_active_USOS', 'nazwisko', 'imie', 'skreslony', 'rezygnacja')
         import_id_fields = ['indeks']
